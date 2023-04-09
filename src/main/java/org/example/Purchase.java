1e89a39c5a4c3943033d4ae18836e3f88fe40965
nikalex777 <777_nikolaev@mail.ru>
2023-04-09 15:15:35 +0300

    corrections

---
+++ src/main/java/org/example/Purchase.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Purchase {
+    protected String title;
+    protected int count;
+
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+
+}
